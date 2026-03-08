5205a2c5b9145d947a7c996d4bbc71606fbe9cba
Disha Sikder <sikderdisha715@gmail.com>
2026-03-08 18:47:48 +0600

    add money,home page,cash out done

--- scripts/AddMoney.js
+++ scripts/AddMoney.js
@@ -1,50 +1,54 @@
-document.getElementById("btn-addMoney").addEventListener('click',function(){
-    //console.log('add money button clicked');
+document.getElementById("btn-addMoney").addEventListener("click", function () {
+  //console.log('add money button clicked');
 
-    //1.get the bank
-    const SelectBankAccount =getValueFromInput("add-money-bank");
-    //console.log(SelectBankAccount);
+  //1.get the bank
+  const SelectBankAccount = getValueFromInput("add-money-bank");
+  //console.log(SelectBankAccount);
 
-    //1.1 validation
-    if(SelectBankAccount == 'Select back'){
-        alert('Please select the bank.');
-        return;
-    }
-    else{
-console.log("Bank name: ",SelectBankAccount);
-    }
+  //1.1 validation
+  if (SelectBankAccount == "Select back") {
+    alert("Please select the bank.");
+    return;
+  } else {
+    console.log("Bank name: ", SelectBankAccount);
+  }
 
-    //2.get the account number
-    const accountNumber =getValueFromInput("account-number");
-    
+  //2.get the account number
+  const accountNumber = getValueFromInput("account-number");
 
-    //2.1 validation
-    if(accountNumber.length <12 || accountNumber.length > 16){
-        alert("Invalid account number");
-        return;
-    }
+  //2.1 validation
+  if (accountNumber.length < 12 || accountNumber.length > 16) {
+    alert("Invalid account number");
+    return;
+  }
 
-    //3.get the current balance
+  //3.get the current balance
   const currentBalance = getBalance();
 
-    //4.Add amount
-    const AddMoney = getValueFromInput("add-money-amount");
+  //4.Add amount
+  const AddMoney = getValueFromInput("add-money-amount");
 
-    //5.new amount
-    const newAmount = currentBalance + Number(AddMoney);
-    console.log(newAmount);
+  //5.new amount
+  const newAmount = currentBalance + Number(AddMoney);
+  console.log(newAmount);
 
-    //6.get the pin
-    const pin = getValueFromInput("pin");
+  //6.get the pin
+  const pin = getValueFromInput("add-pin");
 
-    //6.1validation
-    if(pin == '1234'){
-        alert("$" + AddMoney + " added successfully to your account.");
-        //6.2.set new amount to current amount
-        document.getElementById("balance").innerText=newAmount;
-    }
-    else{
-        alert('Invalid pin.');
-        return;
-    }
-})
+  //6.1validation
+  if (pin == "1234") {
+    // alert("$" + AddMoney + " added successfully to your account from " + SelectBankAccount + "at " + new Date());
+
+    alert(`
+${AddMoney} Taka added successfully to your account
+from ${SelectBankAccount}
+at ${new Date()}
+`);
+    //6.2.set new amount to current amount
+    document.getElementById("balance").innerText = newAmount;
+  } 
+  else {
+    alert("Invalid pin.");
+    return;
+  }
+});
--- scripts/machine.js
+++ scripts/machine.js
@@ -16,3 +16,22 @@ function getBalance(){
     return Number(balance);
 
 }
+
+//id > hide all sections except that one i clicked
+function showOnly(id){
+  console.log('show only clicked.')
+  //get the id's
+const addMoney = document.getElementById("add-money");
+const payments = document.getElementById("latest-payment");
+const cashOut =document.getElementById("cash-out");
+
+//make all section hidden
+addMoney.classList.add('hidden');
+cashOut.classList.add('hidden');
+
+//remove hidden from clicked btn
+//get id
+const selectBtn = document.getElementById(id);
+selectBtn.classList.remove('hidden');
+
+}
